b80e91f56c008640c9f30b1142d1fb762cd0a6df
Sérgio Marcelino <sergiofilhow@gmail.com>
2019-06-13 16:46:29 -0300

    some changes to facilitate the plugin disable

--- keycloak-plugin/src/main/java/org/entando/entando/keycloak/services/UserManager.java
+++ keycloak-plugin/src/main/java/org/entando/entando/keycloak/services/UserManager.java
@@ -1,56 +1,97 @@
 package org.entando.entando.keycloak.services;
 
+import com.agiletec.aps.system.exception.ApsSystemException;
+import com.agiletec.aps.system.services.authorization.Authorization;
+import com.agiletec.aps.system.services.authorization.IAuthorizationManager;
 import com.agiletec.aps.system.services.user.IUserManager;
 import com.agiletec.aps.system.services.user.User;
 import com.agiletec.aps.system.services.user.UserDetails;
-import org.entando.entando.aps.system.services.user.IUserService;
-import org.entando.entando.web.user.model.UserRequest;
+import org.apache.commons.lang.StringUtils;
+import org.entando.entando.KeycloakWiki;
+import org.entando.entando.aps.system.exception.ResourceNotFoundException;
+import org.entando.entando.aps.system.exception.RestServerError;
+import org.entando.entando.keycloak.services.oidc.OpenIDConnectService;
+import org.entando.entando.keycloak.services.oidc.exception.CredentialsExpiredException;
+import org.entando.entando.keycloak.services.oidc.exception.OidcException;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+import static org.entando.entando.KeycloakWiki.wiki;
 
 public class UserManager implements IUserManager {
 
-    @Autowired private UserService userService;
+    private static final String ERRCODE_USER_NOT_FOUND = "1";
+    private static final Logger log = LoggerFactory.getLogger(UserManager.class);
+
+    private final IAuthorizationManager authorizationManager;
+    private final KeycloakService keycloakService;
+    private final OpenIDConnectService oidcService;
+
+    @Autowired
+    public UserManager(final IAuthorizationManager authorizationManager,
+                       final KeycloakService keycloakService,
+                       final OpenIDConnectService oidcService) {
+        this.authorizationManager = authorizationManager;
+        this.keycloakService = keycloakService;
+        this.oidcService = oidcService;
+    }
 
     @Override
     public List<String> getUsernames() {
-        return userService.getUsernames();
+        return keycloakService.getRealmResource().users().list().stream()
+                .map(UserRepresentation::getUsername)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<String> searchUsernames(final String text) {
-        return userService.searchUsernames(text);
+        return list(text)
+                .map(UserRepresentation::getUsername)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<UserDetails> getUsers() {
-        return userService.getUsers();
+        return keycloakService.getRealmResource().users().list().stream()
+                .map(KeycloakMapper::convertUserDetails)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<UserDetails> searchUsers(final String text) {
-        return userService.searchUsers(text);
+        return list(text)
+                .map(KeycloakMapper::convertUserDetails)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void removeUser(final UserDetails user) {
-        userService.removeUser(user.getUsername());
+        removeUser(user.getUsername());
     }
 
     @Override
     public void removeUser(final String username) {
-        userService.removeUser(username);
+        keycloakService.getRealmResource().users().get(getUserRepresentation(username).getId()).remove();
     }
 
     @Override
     public void updateUser(final UserDetails user) {
-        final UserRequest request = new UserRequest();
-        request.setPassword(user.getPassword());
-        request.setReset(true);
-        request.setStatus(user.isDisabled() ? IUserService.STATUS_DISABLED: IUserService.STATUS_ACTIVE);
-        request.setUsername(user.getUsername());
-        userService.updateUser(request);
+        final UserRepresentation userRep = getUserRepresentation(user.getUsername());
+        userRep.setEnabled(!user.isDisabled());
+        ofNullable(user.getPassword()).ifPresent(password -> updateUserPassword(userRep, password, true));
+        keycloakService.getRealmResource().users().get(userRep.getId()).update(userRep);
     }
 
     @Override
@@ -60,27 +101,48 @@ public class UserManager implements IUserManager {
 
     @Override
     public void changePassword(final String username, final String password) {
-        userService.updateUserPassword(username, password);
+        final UserRepresentation user = getUserRepresentation(username);
+        updateUserPassword(user, password, false);
     }
 
     @Override
     public void addUser(final UserDetails user) {
-        final UserRequest request = new UserRequest();
-        request.setPassword(user.getPassword());
-        request.setReset(true);
-        request.setStatus(user.isDisabled() ? IUserService.STATUS_DISABLED: IUserService.STATUS_ACTIVE);
-        request.setUsername(user.getUsername());
-        userService.addUser(request);
+        UserRepresentation userRep = new UserRepresentation();
+        userRep.setUsername(user.getUsername());
+        userRep.setEnabled(!user.isDisabled());
+
+        final Response response = keycloakService.getRealmResource().users().create(userRep);
+        if (response.getStatus() == 201) {
+            userRep.setId(response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1"));
+            updateUserPassword(userRep, user.getPassword(), true);
+        }
     }
 
     @Override
     public UserDetails getUser(final String username) {
-        return userService.getUserDetails(username);
+        try {
+            final User userDetails = KeycloakMapper.convertUserDetails(getUserRepresentation(username));
+            final List<Authorization> authorizations = authorizationManager.getUserAuthorizations(username);
+
+            userDetails.setAuthorizations(authorizations);
+            return userDetails;
+        } catch (ApsSystemException e) {
+            log.error("Error in loading user {}", username, e);
+            throw new RestServerError("Error in loading user", e);
+        }
     }
 
     @Override
     public UserDetails getUser(final String username, final String password) {
-        return userService.getUser(username, password);
+        try {
+            return ofNullable(oidcService.login(username, password))
+                    .map(token -> getUser(username))
+                    .orElse(null);
+        } catch (CredentialsExpiredException e) {
+            return getUser(username);
+        } catch (OidcException e) {
+            return null;
+        }
     }
 
     @Override
@@ -100,7 +162,43 @@ public class UserManager implements IUserManager {
         return false;
     }
 
-    public void setUserService(final UserService userService) {
-        this.userService = userService;
+    private Stream<UserRepresentation> list(final String text) {
+        final List<UserRepresentation> list = StringUtils.isEmpty(text)
+                ? keycloakService.getRealmResource().users().list()
+                : keycloakService.getRealmResource().users().search(text);
+        // workaround to a bug on keycloak to not list Service Account Users
+        return list.stream().filter(usr -> !usr.getUsername().startsWith("service-account-"));
     }
+
+    private void updateUserPassword(final UserRepresentation user, final String password, final boolean temporary) {
+        final CredentialRepresentation credentials = new CredentialRepresentation();
+        credentials.setValue(password);
+        credentials.setTemporary(temporary);
+        credentials.setType("password");
+        keycloakService.getRealmResource().users().get(user.getId()).resetPassword(credentials);
+
+        if (!temporary) {
+            user.setRequiredActions(emptyList());
+            keycloakService.getRealmResource().users().get(user.getId()).update(user);
+        }
+    }
+
+    private UserRepresentation getUserRepresentation(final String username) {
+        try {
+            return keycloakService.getRealmResource().users().search(username).stream()
+                    .findFirst()
+                    .map(usr -> keycloakService.getRealmResource().users().get(usr.getId()).toRepresentation())
+                    .orElseThrow(() -> new ResourceNotFoundException(ERRCODE_USER_NOT_FOUND, "user", username));
+        } catch (ClientErrorException e) {
+            if (HttpStatus.FORBIDDEN.value() == e.getResponse().getStatus()
+                    || HttpStatus.UNAUTHORIZED.value() == e.getResponse().getStatus()) {
+                throw new RestServerError("There was an error while trying to load user because the " +
+                        "client on Keycloak doesn't have permission to do that. " +
+                        "The client needs to have Service Accounts enabled and the permission 'realm-admin' on client 'realm-management'. " +
+                        "For more details, refer to the wiki " + wiki(KeycloakWiki.EN_APP_CLIENT_FORBIDDEN), e);
+            }
+            throw e;
+        }
+    }
+
 }
---
+++ keycloak-plugin/src/test/java/org/entando/entando/keycloak/services/UserManagerIntegrationTest.java
@@ -0,0 +1,183 @@
+package org.entando.entando.keycloak.services;
+
+import com.agiletec.aps.system.exception.ApsSystemException;
+import com.agiletec.aps.system.services.authorization.IAuthorizationManager;
+import com.agiletec.aps.system.services.user.User;
+import com.agiletec.aps.system.services.user.UserDetails;
+import org.entando.entando.aps.system.exception.RestServerError;
+import org.entando.entando.keycloak.KeycloakTestConfiguration;
+import org.entando.entando.keycloak.services.oidc.OpenIDConnectService;
+import org.entando.entando.web.user.model.UserPasswordRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+
+public class UserManagerIntegrationTest {
+
+    private static final String USERNAME = "marine";
+
+    @Mock private IAuthorizationManager authorizationManager;
+
+    private UserManager userManager;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        final KeycloakConfiguration configuration = KeycloakTestConfiguration.getConfiguration();
+        final KeycloakService keycloakService = new KeycloakService(configuration);
+        final OpenIDConnectService oidcService = new OpenIDConnectService(configuration);
+        userManager = new UserManager(authorizationManager, keycloakService, oidcService);
+
+        KeycloakTestConfiguration.deleteUsers();
+    }
+
+    @Test
+    public void testUsers() {
+        assertThat(userManager.getUsers()).isEmpty();
+
+        userManager.addUser(activeUser());
+
+        List<UserDetails> users = userManager.getUsers();
+        assertThat(users).hasSize(1);
+        assertThat(users.get(0)).hasFieldOrPropertyWithValue("username", USERNAME);
+        assertThat(users.get(0)).hasFieldOrPropertyWithValue("disabled", false);
+
+        assertThat(userManager.getUsernames()).hasSize(1).contains(USERNAME);
+        assertThat(userManager.searchUsernames(USERNAME)).hasSize(1).contains(USERNAME);
+        assertThat(userManager.searchUsernames("")).hasSize(1).contains(USERNAME);
+        assertThat(userManager.searchUsernames(null)).hasSize(1).contains(USERNAME);
+        assertThat(userManager.searchUsernames("aa")).isEmpty();
+
+        users = userManager.searchUsers(USERNAME);
+        assertThat(users).hasSize(1);
+        assertThat(users.get(0)).hasFieldOrPropertyWithValue("username", USERNAME);
+        assertThat(users.get(0)).hasFieldOrPropertyWithValue("disabled", false);
+
+        users = userManager.searchUsers("");
+        assertThat(users).hasSize(1);
+
+        assertThat(userManager.searchUsers("aa")).isEmpty();
+
+        userManager.removeUser(USERNAME);
+        assertThat(userManager.getUsers()).isEmpty();
+        assertThat(userManager.getUsernames()).isEmpty();
+        assertThat(userManager.searchUsers(USERNAME)).isEmpty();
+        assertThat(userManager.searchUsernames(USERNAME)).isEmpty();
+    }
+
+    @Test
+    public void testGetUser() {
+        userManager.addUser(activeUser());
+        final UserDetails user = userManager.getUser(USERNAME);
+        assertThat(user).hasFieldOrPropertyWithValue("username", USERNAME)
+                .hasFieldOrPropertyWithValue("disabled", false)
+                .hasFieldOrPropertyWithValue("accountNotExpired", true)
+                .hasFieldOrPropertyWithValue("credentialsNotExpired", false);
+    }
+
+    @Test
+    public void testGetUsers() {
+        userManager.addUser(activeUser());
+        userManager.addUser(activeUser("cop"));
+        userManager.addUser(activeUser("doomguy"));
+
+        final List<String> usernames = userManager.getUsernames();
+        final List<String> searchUsernames = userManager.searchUsernames("mar");
+        final List<String> noUsernames = userManager.searchUsernames("1010");
+
+        assertThat(usernames).hasSize(3).contains(USERNAME, "cop", "doomguy");
+        assertThat(searchUsernames).hasSize(1).contains(USERNAME);
+        assertThat(noUsernames).isEmpty();
+    }
+
+    @Test
+    public void testAddUserAndAuthenticate() {
+        userManager.addUser(activeUser());
+
+        UserDetails user = userManager.getUser(USERNAME, "qwer1234");
+        assertThat(user).isNotNull();
+        assertThat(user.isCredentialsNotExpired()).isFalse();
+
+        assertThat(userManager.getUser(USERNAME, "1234qwer")).isNull();
+
+        userManager.changePassword(USERNAME, "4321rewq");
+        user = userManager.getUser(USERNAME, "4321rewq");
+        assertThat(user).isNotNull();
+        assertThat(user.isCredentialsNotExpired()).isTrue();
+    }
+
+    @Test
+    public void testUserUpdatePassword() {
+        userManager.addUser(activeUser());
+
+        UserDetails user = userManager.getUser(USERNAME, "qwer1234");
+        assertThat(user).isNotNull();
+        assertThat(user.isCredentialsNotExpired()).isFalse();
+
+        final UserPasswordRequest request = new UserPasswordRequest();
+        request.setOldPassword("qwer1234");
+        request.setNewPassword("1234qwer");
+        request.setUsername(USERNAME);
+        userManager.changePassword(USERNAME, "1234qwer");
+
+        user = userManager.getUser(USERNAME, "1234qwer");
+        assertThat(user).isNotNull();
+        assertThat(user.isCredentialsNotExpired()).isTrue();
+    }
+
+    @Test
+    public void testUserUpdate() {
+        final User request = (User) activeUser();
+        userManager.addUser(request);
+        assertThat(userManager.getUser(USERNAME, "qwer1234")).isNotNull();
+
+        request.setDisabled(true);
+        userManager.updateUser(request);
+        assertThat(userManager.getUser(USERNAME, "qwer1234")).isNull();
+    }
+
+    @Test
+    public void testAddDisabledUserAndAuthenticate() {
+        userManager.addUser(disabledUser());
+        assertThat(userManager.getUser(USERNAME, "qwer1234")).isNull();
+    }
+
+    @Test(expected = RestServerError.class)
+    public void testGetUserDetailsException() throws ApsSystemException {
+        userManager.addUser(activeUser());
+        doThrow(new ApsSystemException(USERNAME)).when(authorizationManager).getUserAuthorizations(anyString());
+        userManager.getUser(USERNAME);
+    }
+
+    private static UserDetails activeUser() {
+        return activeUser(USERNAME);
+    }
+
+    private static UserDetails activeUser(final String username) {
+        return user(username, "qwer1234", true);
+    }
+
+    static UserDetails activeUser(final String username, final String password) {
+        return user(username, password, true);
+    }
+
+    private static UserDetails user(final String username, final String password, final boolean enabled) {
+        final User request = new User();
+        request.setDisabled(!enabled);
+        request.setUsername(username);
+        request.setPassword(password);
+        return request;
+    }
+
+    private static UserDetails disabledUser() {
+        return user(USERNAME, "qwer1234", false);
+    }
+
+}
