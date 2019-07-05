4c8e763463172809c32a45ffb29138097ff61fc5
Ampie Barnard <a.barnard@entando.com>
2019-07-05 17:31:46 +0200

    Fixed jacoco report extraction

--- keycloak-plugin/run-tests.sh
+++ keycloak-plugin/run-tests.sh
@@ -4,7 +4,7 @@ mvn clean package -DskipTests
 docker-compose -f docker-compose-cicd.yml build
 docker-compose -f docker-compose-cicd.yml up -d keycloak
 docker-compose -f docker-compose-cicd.yml up entando-keycloak-test
-docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/site ./core/target/
-docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/surefire-reports ./core/target/
-docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/failsafe-reports ./core/target/
+docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/site ./test-result/
+docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/surefire-reports ./test-result/
+docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/failsafe-reports ./test-result/
 docker-compose -f docker-compose-cicd.yml down
