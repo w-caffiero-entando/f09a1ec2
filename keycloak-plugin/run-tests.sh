23acb886b3709a25242a988667a28b7a5d560b59
Ampie Barnard <a.barnard@entando.com>
2019-07-08 09:08:19 +0200

    Bound Jacoco reporting to verify phase

--- keycloak-plugin/run-tests.sh
+++ keycloak-plugin/run-tests.sh
@@ -4,7 +4,5 @@ docker-compose -f docker-compose-cicd.yml build
 docker-compose -f docker-compose-cicd.yml up -d keycloak
 docker-compose -f docker-compose-cicd.yml up entando-keycloak-test
 mkdir -p ./test-result
-docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/site ./test-result/
-docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/surefire-reports ./test-result/
-docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target/failsafe-reports ./test-result/
+docker cp $(docker ps -aq --filter ancestor=entando-keycloak-test:latest):/usr/src/entando-keycloak-plugin/core/target ./core-docker-cicd-result/
 docker-compose -f docker-compose-cicd.yml down
