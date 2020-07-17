5da8c62b9bd0828e85100deb3269ffc770e8da47
Ampie Barnard <ampie.private@gmail.com>
2020-07-17 10:34:09 +0200

    ENG-572 initial commit before importing to jx

--- keycloak-plugin/src/test/java/org/entando/entando/keycloak/KeycloakTestConfiguration.java
+++ keycloak-plugin/src/test/java/org/entando/entando/keycloak/KeycloakTestConfiguration.java
@@ -9,6 +9,7 @@ import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.ClientResource;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RoleScopeResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
@@ -16,6 +17,7 @@ import org.keycloak.representations.idm.RoleRepresentation;
 
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
@@ -24,7 +26,7 @@ import static java.util.Optional.ofNullable;
 public class KeycloakTestConfiguration {
 
     private static final String BASE_URL = ofNullable(System.getenv("KEYCLOAK_AUTH_URL")).orElse("http://localhost:8081/auth");
-    private static final String REALM_NAME = "entando-core-test";
+    private static final String REALM_NAME = "entando-development";
     private static final String CLIENT_ID = "entando-core";
 
     private static KeycloakService keycloakService;
@@ -52,7 +54,7 @@ public class KeycloakTestConfiguration {
                 .grantType(OAuth2Constants.PASSWORD)
                 .realm("master")
                 .clientId("admin-cli")
-                .username("admin")
+                .username("entando-admin")
                 .password("qwe123")
                 .build();
 
@@ -70,8 +72,11 @@ public class KeycloakTestConfiguration {
 
     private static void assignRoleRealmAdmin(final RealmResource resource, final ClientResource clientResource) {
         final String id = resource.clients().findByClientId("realm-management").get(0).getId();
-        final RoleRepresentation role = resource.clients().get(id).roles().get("realm-admin").toRepresentation();
-        resource.users().get(clientResource.getServiceAccountUser().getId()).roles().clientLevel(id).add(singletonList(role));
+        RoleScopeResource rolesByClient = resource.users().get(clientResource.getServiceAccountUser().getId()).roles().clientLevel(id);
+        if(rolesByClient.listAll().isEmpty()) {
+            final RoleRepresentation role = resource.clients().get(id).roles().get("realm-admin").toRepresentation();
+            rolesByClient.add(singletonList(role));
+        }
     }
 
     private static String getClientSecret(final Keycloak keycloak) {
@@ -79,7 +84,9 @@ public class KeycloakTestConfiguration {
             final RealmResource realm = keycloak.realm(REALM_NAME);
             realm.toRepresentation();
             final List<ClientRepresentation> id = realm.clients().findByClientId(CLIENT_ID);
-            return realm.clients().get(id.get(0).getId()).getSecret().getValue();
+            ClientResource clientResource = realm.clients().get(id.get(0).getId());
+            assignRoleRealmAdmin(realm, clientResource);
+            return clientResource.getSecret().getValue();
         } catch (final NotFoundException e) {
             return createRealm(keycloak);
         }
@@ -92,6 +99,20 @@ public class KeycloakTestConfiguration {
         newRealm.setDisplayName("Entando - Test");
         masterKeycloak.realms().create(newRealm);
 
+
+        final RealmResource resource = masterKeycloak.realm(REALM_NAME);
+        createEntandoWebClient(resource);
+        final Response response = createEntandoCoreClient(resource);
+        final String id = response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1");
+        final ClientResource clientResource = resource.clients().get(id);
+        final String secret = clientResource.generateNewSecret().getValue();
+
+        assignRoleRealmAdmin(resource, clientResource);
+
+        return secret;
+    }
+
+    private static Response createEntandoCoreClient(RealmResource resource) {
         final ClientRepresentation client = new ClientRepresentation();
         client.setName("entando-core");
         client.setClientId(CLIENT_ID);
@@ -102,16 +123,24 @@ public class KeycloakTestConfiguration {
         client.setDirectAccessGrantsEnabled(true);
         client.setAuthorizationServicesEnabled(false);
         client.setRedirectUris(singletonList("http://localhost:8080/*"));
+        return resource.clients().create(client);
+    }
 
-        final RealmResource resource = masterKeycloak.realm(REALM_NAME);
-        final Response response = resource.clients().create(client);
-        final String id = response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1");
-        final ClientResource clientResource = resource.clients().get(id);
-        final String secret = clientResource.generateNewSecret().getValue();
-
-        assignRoleRealmAdmin(resource, clientResource);
-
-        return secret;
+    private static void createEntandoWebClient(RealmResource realmResource) {
+        ClientRepresentation client = new ClientRepresentation();
+        client.setName("Entando WEB");
+        client.setClientId("entando-web");
+        client.setEnabled(true);
+        client.setServiceAccountsEnabled(false);
+        client.setStandardFlowEnabled(true);
+        client.setImplicitFlowEnabled(true);
+        client.setDirectAccessGrantsEnabled(false);
+        client.setAuthorizationServicesEnabled(false);
+        client.setRedirectUris(Collections.singletonList("*"));
+        client.setPublicClient(true);
+        client.setOrigin("*");
+        client.setWebOrigins(Collections.singletonList("*"));
+        realmResource.clients().create(client);
     }
 
 }
