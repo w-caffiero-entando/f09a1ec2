23636cd5ef27d443eb09740a7b82d25c08146afe
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2022-10-31 11:14:54 +0100

    ENG-4183: Fix indentation

--- keycloak-plugin/src/main/java/org/entando/entando/keycloak/services/control/KcRequestAuthorizator.java
+++ keycloak-plugin/src/main/java/org/entando/entando/keycloak/services/control/KcRequestAuthorizator.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2022-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package org.entando.entando.keycloak.services.control;
 
 import com.agiletec.aps.system.RequestContext;
@@ -16,41 +29,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KcRequestAuthorizator extends RequestAuthorizator {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(KcRequestAuthorizator.class);
-    
+
     private boolean enabled;
-    
+
     @Override
-	public int service(RequestContext reqCtx, int status) {
+    public int service(RequestContext reqCtx, int status) {
         if (!this.isEnabled()) {
             return super.service(reqCtx, status);
         }
-		logger.debug("Invoked: {}", this.getClass().getName());
-		int retStatus = ControllerManager.INVALID_STATUS;
-		if (status == ControllerManager.ERROR) {
-			return status;
-		}
-		try {
-			HttpServletRequest req = reqCtx.getRequest();
-			HttpSession session = req.getSession();
-			IPage currentPage = (IPage) reqCtx.getExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE);
-			UserDetails currentUser = (UserDetails) session.getAttribute(SystemConstants.SESSIONPARAM_CURRENT_USER);
-			if (null == currentUser) {
-				throw new EntException("no user on session");
-			}
-			boolean authorized = this.getAuthManager().isAuth(currentUser, currentPage);
-			if (authorized) {
-				retStatus = ControllerManager.CONTINUE;
-			} else if (!currentUser.getUsername().equalsIgnoreCase(SystemConstants.GUEST_USER_NAME)) {
+        logger.debug("Invoked: {}", this.getClass().getName());
+        if (status == ControllerManager.ERROR) {
+            return status;
+        }
+        int retStatus = ControllerManager.INVALID_STATUS;
+        try {
+            HttpServletRequest req = reqCtx.getRequest();
+            HttpSession session = req.getSession();
+            IPage currentPage = (IPage) reqCtx.getExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE);
+            UserDetails currentUser = (UserDetails) session.getAttribute(SystemConstants.SESSIONPARAM_CURRENT_USER);
+            if (null == currentUser) {
+                throw new EntException("no user on session");
+            }
+            boolean authorized = this.getAuthManager().isAuth(currentUser, currentPage);
+            if (authorized) {
+                retStatus = ControllerManager.CONTINUE;
+            } else if (!currentUser.getUsername().equalsIgnoreCase(SystemConstants.GUEST_USER_NAME)) {
                 return this.returnUserNotAuthorized(reqCtx);
             } else {
                 StringBuilder targetUrl = new StringBuilder(req.getRequestURL());
                 targetUrl.append("?");
-				String queryString = req.getQueryString();
-				if (null != queryString && queryString.trim().length() > 0) {
-					targetUrl.append(queryString).append("&");
-				}
+                String queryString = req.getQueryString();
+                if (null != queryString && queryString.trim().length() > 0) {
+                    targetUrl.append(queryString).append("&");
+                }
                 targetUrl.append(RequestContext.PAR_REDIRECT_FLAG).append("=1");
                 String kcEntryPoint = this.getUrlManager().getApplicationBaseURL(req);
                 if (!kcEntryPoint.endsWith("/")) {
@@ -58,34 +71,34 @@ public class KcRequestAuthorizator extends RequestAuthorizator {
                 }
                 kcEntryPoint += "do/login?redirectTo=" + URLEncoder.encode(targetUrl.toString(), "UTF-8");
                 retStatus = this.redirectKcUrl(kcEntryPoint, reqCtx);
-			}
-		} catch (Exception t) {
-			logger.error("Error while processing the request", t);
-			retStatus = ControllerManager.SYS_ERROR;
-			reqCtx.setHTTPError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		return retStatus;
-	}
-    
+            }
+        } catch (Exception t) {
+            logger.error("Error while processing the request", t);
+            retStatus = ControllerManager.SYS_ERROR;
+            reqCtx.setHTTPError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        return retStatus;
+    }
+
     protected int redirectKcUrl(String urlDest, RequestContext reqCtx) {
-		int retStatus;
-		try {
+        int retStatus;
+        try {
             String redirPar = this.getParameter(RequestContext.PAR_REDIRECT_FLAG, reqCtx);
-			if (redirPar == null || "".equals(redirPar)) {
+            if (redirPar == null || "".equals(redirPar)) {
                 reqCtx.clearError();
                 reqCtx.addExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL, urlDest);
                 retStatus = ControllerManager.REDIRECT;
             } else {
                 return this.returnUserNotAuthorized(reqCtx);
-			}
-		} catch (Exception t) {
-			retStatus = ControllerManager.SYS_ERROR;
-			reqCtx.setHTTPError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			logger.error("Error on creation redirect to url {}", urlDest, t);
-		}
-		return retStatus;
-	}
-    
+            }
+        } catch (Exception t) {
+            retStatus = ControllerManager.SYS_ERROR;
+            reqCtx.setHTTPError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            logger.error("Error on creation redirect to url {}", urlDest, t);
+        }
+        return retStatus;
+    }
+
     protected int returnUserNotAuthorized(RequestContext reqCtx) {
         reqCtx.getRequest().setAttribute("userUnauthorized", true);
         reqCtx.setHTTPError(HttpServletResponse.SC_UNAUTHORIZED);
@@ -99,8 +112,9 @@ public class KcRequestAuthorizator extends RequestAuthorizator {
     protected boolean isEnabled() {
         return enabled;
     }
+
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
-    
+
 }
--- keycloak-plugin/src/test/java/org/entando/entando/keycloak/services/control/KcRequestAuthorizatorTest.java
+++ keycloak-plugin/src/test/java/org/entando/entando/keycloak/services/control/KcRequestAuthorizatorTest.java
@@ -20,104 +20,104 @@ import org.springframework.mock.web.MockHttpServletRequest;
  * @author E.Santoboni
  */
 class KcRequestAuthorizatorTest extends BaseTestCase {
-    
+
     @Test
     void testService_1() throws Throwable {
-        ((KcRequestAuthorizator) this._authorizator).setEnabled(false);
-		this.executeTestService_1();
-        ((KcRequestAuthorizator) this._authorizator).setEnabled(true);
+        ((KcRequestAuthorizator) this.authorizator).setEnabled(false);
         this.executeTestService_1();
-	}
-    
-    private void executeTestService_1() throws Throwable {
-		RequestContext reqCtx = this.getRequestContext();
-		this.setUserOnSession(SystemConstants.GUEST_USER_NAME);
-		IPage root = this._pageManager.getOnlineRoot();
-		reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, root);
-		int status = _authorizator.service(reqCtx, ControllerManager.CONTINUE);
-		assertEquals(ControllerManager.CONTINUE, status);
-		String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
-		assertNull(redirectUrl);
-	}
+        ((KcRequestAuthorizator) this.authorizator).setEnabled(true);
+        this.executeTestService_1();
+    }
 
-	@Test
+    private void executeTestService_1() throws Throwable {
+        RequestContext reqCtx = this.getRequestContext();
+        this.setUserOnSession(SystemConstants.GUEST_USER_NAME);
+        IPage root = this.pageManager.getOnlineRoot();
+        reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, root);
+        int status = this.authorizator.service(reqCtx, ControllerManager.CONTINUE);
+        assertEquals(ControllerManager.CONTINUE, status);
+        String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
+        assertNull(redirectUrl);
+    }
+
+    @Test
     void testService_2() throws Throwable {
-		RequestContext reqCtx = this.getRequestContext();
-		this.setUserOnSession("admin");
-		IPage root = this._pageManager.getOnlineRoot();
-		reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, root);
-		int status = this._authorizator.service(reqCtx, ControllerManager.CONTINUE);
-		assertEquals(ControllerManager.CONTINUE, status);
-		String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
-		assertNull(redirectUrl);
-	}
-    
+        RequestContext reqCtx = this.getRequestContext();
+        this.setUserOnSession("admin");
+        IPage root = this.pageManager.getOnlineRoot();
+        reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, root);
+        int status = this.authorizator.service(reqCtx, ControllerManager.CONTINUE);
+        assertEquals(ControllerManager.CONTINUE, status);
+        String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
+        assertNull(redirectUrl);
+    }
+
     @Test
     void testServiceError() throws Throwable {
         RequestContext reqCtx = this.getRequestContext();
-		int status = _authorizator.service(reqCtx, ControllerManager.ERROR);
-		assertEquals(ControllerManager.ERROR, status);
-	}
+        int status = this.authorizator.service(reqCtx, ControllerManager.ERROR);
+        assertEquals(ControllerManager.ERROR, status);
+    }
 
-	@Test
+    @Test
     void testServiceFailure_1() throws Throwable {
-		RequestContext reqCtx = this.getRequestContext();
-		((MockHttpServletRequest) reqCtx.getRequest()).setRequestURI("/Entando/it/customers_page.page");
-		((MockHttpServletRequest) reqCtx.getRequest()).setQueryString("qsparamname=qsparamvalue");
-		this.setUserOnSession(SystemConstants.GUEST_USER_NAME);
-		IPage requiredPage = this._pageManager.getOnlinePage("customers_page");
-		reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, requiredPage);
-		int status = _authorizator.service(reqCtx, ControllerManager.CONTINUE);
-		assertEquals(ControllerManager.REDIRECT, status);
-		String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
-		assertTrue(redirectUrl.contains("/Entando/do/login?"));
-		assertTrue(redirectUrl.contains("redirectflag"));
-		assertTrue(redirectUrl.contains("redirectTo="));
-		assertTrue(redirectUrl.contains("customers_page.page"));
-		assertTrue(redirectUrl.contains("qsparamname"));
-		assertTrue(redirectUrl.contains("qsparamvalue"));
-	}
+        RequestContext reqCtx = this.getRequestContext();
+        ((MockHttpServletRequest) reqCtx.getRequest()).setRequestURI("/Entando/it/customers_page.page");
+        ((MockHttpServletRequest) reqCtx.getRequest()).setQueryString("qsparamname=qsparamvalue");
+        this.setUserOnSession(SystemConstants.GUEST_USER_NAME);
+        IPage requiredPage = this.pageManager.getOnlinePage("customers_page");
+        reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, requiredPage);
+        int status = this.authorizator.service(reqCtx, ControllerManager.CONTINUE);
+        assertEquals(ControllerManager.REDIRECT, status);
+        String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
+        assertTrue(redirectUrl.contains("/Entando/do/login?"));
+        assertTrue(redirectUrl.contains("redirectflag"));
+        assertTrue(redirectUrl.contains("redirectTo="));
+        assertTrue(redirectUrl.contains("customers_page.page"));
+        assertTrue(redirectUrl.contains("qsparamname"));
+        assertTrue(redirectUrl.contains("qsparamvalue"));
+    }
 
-	@Test
+    @Test
     void testServiceFailure_2() throws Throwable {
-		RequestContext reqCtx = this.getRequestContext();
-		reqCtx.getRequest().getSession().removeAttribute(SystemConstants.SESSIONPARAM_CURRENT_USER);
-		IPage root = this._pageManager.getOnlineRoot();
-		reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, root);
-		int status = _authorizator.service(reqCtx, ControllerManager.CONTINUE);
-		assertEquals(ControllerManager.SYS_ERROR, status);
-	}
-    
-	@Test
+        RequestContext reqCtx = this.getRequestContext();
+        reqCtx.getRequest().getSession().removeAttribute(SystemConstants.SESSIONPARAM_CURRENT_USER);
+        IPage root = this.pageManager.getOnlineRoot();
+        reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, root);
+        int status = this.authorizator.service(reqCtx, ControllerManager.CONTINUE);
+        assertEquals(ControllerManager.SYS_ERROR, status);
+    }
+
+    @Test
     void testUserNotAuthorized() throws Throwable {
-		RequestContext reqCtx = this.getRequestContext();
-		((MockHttpServletRequest) reqCtx.getRequest()).setRequestURI("/Entando/it/coach_page.page");
-		this.setUserOnSession("editorCustomers");
-		IPage requiredPage = this._pageManager.getOnlinePage("coach_page");
-		reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, requiredPage);
-		int status = _authorizator.service(reqCtx, ControllerManager.CONTINUE);
-		assertEquals(ControllerManager.CONTINUE, status);
-		String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
-		assertNull(redirectUrl);
+        RequestContext reqCtx = this.getRequestContext();
+        ((MockHttpServletRequest) reqCtx.getRequest()).setRequestURI("/Entando/it/coach_page.page");
+        this.setUserOnSession("editorCustomers");
+        IPage requiredPage = this.pageManager.getOnlinePage("coach_page");
+        reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE, requiredPage);
+        int status = this.authorizator.service(reqCtx, ControllerManager.CONTINUE);
+        assertEquals(ControllerManager.CONTINUE, status);
+        String redirectUrl = (String) reqCtx.getExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
+        assertNull(redirectUrl);
         assertEquals(HttpServletResponse.SC_UNAUTHORIZED, reqCtx.getResponse().getStatus());
         IPage currentPage = (IPage) reqCtx.getExtraParam(SystemConstants.EXTRAPAR_CURRENT_PAGE);
-		assertEquals("errorpage", currentPage.getCode());
-	}
-    
+        assertEquals("errorpage", currentPage.getCode());
+    }
+
     @BeforeEach
-	private void init() throws Exception {
-		try {
-			this._authorizator = (ControlServiceInterface) this.getApplicationContext().getBean("RequestAuthorizatorControlService");
-			this._pageManager = (IPageManager) this.getService(SystemConstants.PAGE_MANAGER);
+    private void init() throws Exception {
+        try {
+            this.authorizator = (ControlServiceInterface) this.getApplicationContext().getBean("RequestAuthorizatorControlService");
+            this.pageManager = (IPageManager) this.getService(SystemConstants.PAGE_MANAGER);
             super.getRequestContext().removeExtraParam(RequestContext.EXTRAPAR_REDIRECT_URL);
-            ((KcRequestAuthorizator) this._authorizator).setEnabled(true);
-		} catch (Throwable e) {
-			throw new Exception(e);
-		}
-	}
+            ((KcRequestAuthorizator) this.authorizator).setEnabled(true);
+        } catch (Throwable e) {
+            throw new Exception(e);
+        }
+    }
 
-	private ControlServiceInterface _authorizator;
+    private ControlServiceInterface authorizator;
 
-	private IPageManager _pageManager;
-    
+    private IPageManager pageManager;
+
 }
