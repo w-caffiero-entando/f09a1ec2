b5d6fcab96d7018f54e2c39e7fda01441dc7c898
zonia3000 <s.zorba@entando.com>
2023-02-01 12:02:00 +0100

    ENG-4522 Added unit test

---
+++ engine/src/test/java/com/agiletec/aps/system/common/UnitAbstractCacheWrapperTest.java
@@ -0,0 +1,46 @@
+package com.agiletec.aps.system.common;
+
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cache.Cache;
+import org.springframework.cache.support.SimpleValueWrapper;
+
+@ExtendWith(MockitoExtension.class)
+class UnitAbstractCacheWrapperTest {
+
+    private static final String CACHE_NAME = "cache_name";
+
+    @Mock
+    private Cache cache;
+
+    private AbstractCacheWrapper cacheWrapper;
+
+    @BeforeEach
+    void setUp() {
+        cacheWrapper = new AbstractCacheWrapper() {
+            @Override
+            protected String getCacheName() {
+                return CACHE_NAME;
+            }
+        };
+    }
+
+    @Test
+    void getCopyFromImmutableCacheMapShouldHandleNullValue() {
+        Assertions.assertNull(cacheWrapper.getCopyFromImmutableCacheMap(cache, "not_set"));
+    }
+
+    @Test
+    void getCopyFromImmutableCacheMapShouldCreateAMutableCopy() {
+        Mockito.when(cache.get("key")).thenReturn(new SimpleValueWrapper(Map.of("k1", "v1")));
+        Map<String, String> copy = cacheWrapper.getCopyFromImmutableCacheMap(cache, "key");
+        copy.put("k2", "v2");
+        Assertions.assertEquals(2, copy.size());
+    }
+}
