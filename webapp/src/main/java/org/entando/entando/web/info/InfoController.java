1e63547da36013993d87662402737488b243f873
Luigi Sportelli <l.sportelli@entando.com>
2023-03-06 14:56:58 +0100

    ENG-4456 added change from code review

--- webapp/src/main/java/org/entando/entando/web/info/InfoController.java
+++ webapp/src/main/java/org/entando/entando/web/info/InfoController.java
@@ -1,6 +1,7 @@
 package org.entando.entando.web.info;
 
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.entando.entando.ent.util.EntLogging.EntLogFactory;
 import org.entando.entando.ent.util.EntLogging.EntLogger;
 import org.springframework.http.HttpStatus;
@@ -10,21 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/info")
 public class InfoController {
 
-    private final EntLogger logger = EntLogFactory.getSanitizedLogger(getClass());
-
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> info() {
-        logger.debug("retrieving info check");
+        log.debug("retrieving info check");
 
         try {
             Map<String, Object> res = InfoLoader.getInfo();
             return new ResponseEntity<>(res, HttpStatus.OK);
         } catch(Exception ex) {
-            logger.warn("error retrieving info check",ex);
+            log.warn("error retrieving info check",ex);
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
