beed75bdcdeecdbd692c5892cfe24e1622d6867c
zonia3000 <s.zorba@entando.com>
2023-04-20 17:02:16 +0200

    ENG-4778 Replaced Solr flacky integration tests with simpler unit tests

--- solr-plugin/src/test/java/org/entando/entando/plugins/jpsolr/web/config/SolrConfigControllerTest.java
+++ solr-plugin/src/test/java/org/entando/entando/plugins/jpsolr/web/config/SolrConfigControllerTest.java
@@ -13,92 +13,99 @@
  */
 package org.entando.entando.plugins.jpsolr.web.config;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.agiletec.aps.system.services.user.UserDetails;
-import com.jayway.jsonpath.JsonPath;
-import org.entando.entando.plugins.jpsolr.SolrTestExtension;
-import org.entando.entando.plugins.jpsolr.aps.system.solr.ISolrSearchEngineManager;
-import org.entando.entando.plugins.jpsolr.web.AbstractControllerIntegrationTest;
+import java.util.List;
+import org.entando.entando.plugins.jpsolr.aps.system.solr.SolrSearchEngineManager;
+import org.entando.entando.plugins.jpsolr.aps.system.solr.model.ContentTypeSettings;
+import org.entando.entando.web.AbstractControllerTest;
 import org.entando.entando.web.utils.OAuth2TestUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
  * @author E.Santoboni
  */
-@ExtendWith(SolrTestExtension.class)
-@Tag(SolrTestExtension.RECREATE_CORE)
-class SolrConfigControllerTest extends AbstractControllerIntegrationTest {
+@ExtendWith(MockitoExtension.class)
+class SolrConfigControllerTest extends AbstractControllerTest {
 
-    @Autowired
-    private ISolrSearchEngineManager solrSearchEngineManager;
+    @Mock
+    SolrSearchEngineManager searchEngineManager;
 
-    @BeforeAll
-    public static void setup() throws Exception {
-        AbstractControllerIntegrationTest.setup();
+    @InjectMocks
+    private SolrConfigController controller;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .addInterceptors(entandoOauth2Interceptor)
+                .setMessageConverters(getMessageConverters())
+                .setHandlerExceptionResolvers(createHandlerExceptionResolver())
+                .build();
     }
 
     @Test
-    void testRefreshType() throws Exception {
-        UserDetails user = new OAuth2TestUtils.UserBuilder("jack_bauer", "0x24")
-                .grantedToRoleAdmin().build();
+    void shouldGetConfig() throws Exception {
+        UserDetails user = new OAuth2TestUtils.UserBuilder("jack_bauer", "0x24").grantedToRoleAdmin().build();
         String accessToken = mockOAuthInterceptor(user);
 
-        ResultActions result_1 = mockMvc
-                .perform(get("/plugins/solr/config").header("Authorization", "Bearer " + accessToken));
-        result_1.andExpect(status().isOk());
-        String bodyResult_1 = result_1.andReturn().getResponse().getContentAsString();
-        Assertions.assertNotNull(bodyResult_1);
+        ContentTypeSettings settings = new ContentTypeSettings("NWS", "News");
+        Mockito.when(searchEngineManager.getContentTypesSettings()).thenReturn(List.of(settings));
 
-        int payloadSize = JsonPath.read(bodyResult_1, "$.size()");
-        Assertions.assertEquals(4, payloadSize);
-        for (int i = 0; i < payloadSize; i++) {
-            int size = JsonPath.read(bodyResult_1, "$[" + i + "].attributeSettings[0].currentConfig.size()");
-            Assertions.assertEquals(0, size);
-        }
-
-        ResultActions result_2 = mockMvc
-                .perform(post("/plugins/solr/config/TST").header("Authorization", "Bearer " + accessToken));
-        result_2.andExpect(status().isOk());
-        String bodyResult_2 = result_2.andReturn().getResponse().getContentAsString();
-        Assertions.assertNotNull(bodyResult_2);
-
-        ResultActions result_3 = mockMvc
-                .perform(get("/plugins/solr/config").header("Authorization", "Bearer " + accessToken));
-        result_3.andExpect(status().isOk());
-        String bodyResult_3 = result_3.andReturn().getResponse().getContentAsString();
-        Assertions.assertNotNull(bodyResult_3);
-        for (int i = 0; i < payloadSize; i++) {
-            String extractedType = JsonPath.read(bodyResult_3, "$[" + i + "].typeCode");
-            int size = JsonPath.read(bodyResult_3, "$[" + i + "].attributeSettings[0].currentConfig.size()");
-            if (extractedType.equals("TST")) {
-                Assertions.assertTrue(size > 0);
-            } else {
-                Assertions.assertEquals(0, size);
-            }
-        }
-
-        this.solrSearchEngineManager.refreshCmsFields();
-
-        ResultActions result_4 = mockMvc
-                .perform(get("/plugins/solr/config").header("Authorization", "Bearer " + accessToken));
-        result_4.andExpect(status().isOk());
-        String bodyResult_4 = result_4.andReturn().getResponse().getContentAsString();
-        Assertions.assertNotNull(bodyResult_4);
-        for (int i = 0; i < payloadSize; i++) {
-            int size = JsonPath.read(bodyResult_4, "$[" + i + "].attributeSettings[0].currentConfig.size()");
-            Assertions.assertTrue(size > 0);
-        }
-
-
+        ResultActions result = mockMvc.perform(get("/plugins/solr/config")
+                .header("Authorization", "Bearer " + accessToken));
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.payload[0].typeCode", is("NWS")))
+                .andExpect(jsonPath("$.payload[0].typeDescription", is("News")))
+                .andExpect(jsonPath("$.payload[0].valid", is(true)));
     }
 
+    @Test
+    void getConfigShouldReturnForbiddenToNonAdminUser() throws Exception {
+        UserDetails user = new OAuth2TestUtils.UserBuilder("simple_user", "0x24").build();
+        String accessToken = mockOAuthInterceptor(user);
+        ResultActions result = mockMvc.perform(get("/plugins/solr/config")
+                .header("Authorization", "Bearer " + accessToken));
+        result.andExpect(status().isForbidden());
+    }
+
+    @Test
+    void shouldReloadReferences() throws Exception {
+        UserDetails user = new OAuth2TestUtils.UserBuilder("jack_bauer", "0x24").grantedToRoleAdmin().build();
+        String accessToken = mockOAuthInterceptor(user);
+
+        mockMvc.perform(post("/plugins/solr/config/NWS")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.payload.contentTypeCode", is("NWS")))
+                .andExpect(jsonPath("$.payload.status", is("success")));
+
+        Mockito.verify(searchEngineManager, Mockito.times(1)).refreshContentType("NWS");
+    }
+
+    @Test
+    void postConfigShouldReturnForbiddenToNonAdminUser() throws Exception {
+        UserDetails user = new OAuth2TestUtils.UserBuilder("simple_user", "0x24").build();
+        String accessToken = mockOAuthInterceptor(user);
+        ResultActions result = mockMvc.perform(post("/plugins/solr/config/NWS")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + accessToken));
+        result.andExpect(status().isForbidden());
+    }
 }
