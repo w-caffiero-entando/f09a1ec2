b85016ad30828dac9b413fb8286ba02177f6d090
m.panagrosso <m.panagrosso@entando.com>
2023-08-03 17:24:18 +0200

    ENG-5082: changed debug log

--- engine/src/main/java/org/entando/entando/aps/system/services/page/PageService.java
+++ engine/src/main/java/org/entando/entando/aps/system/services/page/PageService.java
@@ -382,8 +382,8 @@ public class PageService implements IComponentExistsService, IPageService,
                 for (String childCode : childCodes) {
                     IPage publicChild = this.getPageManager().getOnlinePage(childCode);
                     if (null != publicChild) {
-                        logger.debug(String.format("Error in delete page with pageCode: '%s' and status: '%s'. "
-                                + "ChildCode: '%s' is online", pageCode, status, childCode));
+                        logger.debug("Error in delete page with pageCode: '{}' and status: '{}'. "
+                                + "ChildCode: '{}' is online", pageCode, status, childCode);
                         bindingResult.reject(PageValidator.ERRCODE_PAGE_WITH_PUBLIC_CHILD,
                                 new String[]{pageCode}, "page.status.publicChild");
                         throw new ValidationGenericException(bindingResult);
